426693aab309161c6f28403de6858c09f6fcd675
Mikola KZ <qazmik23@mail.ru>
2019-01-10 02:25:43 +0600

    'ДобавилMодель,обновилЗависимости'

---
+++ webapp/model.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy 
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db=SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(50),index=True,unique=True)
+    password=db.Column(db.String(128))
+    email=db.Column(db.String(128),unique=True)
+    dateRegistration=(db.String(10))
+    userTelegrammChat=db.Column(db.String(128),unique=True)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+     def __repr__(self):
+        return '<User {}>'.format(self.username)
