ae4d1dd49ac447b81df3cb3f3a5b357d0fd703b6
babushkin.a.l <babushkin.a.l@yandex.ru>
2019-01-13 15:50:05 +0500

    Отключил уведомления

--- webapp/config.py
+++ webapp/config.py
@@ -3,6 +3,7 @@ import os
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI="sqlite:///"+os.path.join(basedir,'..','webapp.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY="sdafsadfqw!@#!F,dfasasdfas25479"
  
