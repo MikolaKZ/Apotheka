1adf60761e71575f1dd5a46f9e00fefa9a4599a9
babushkin.a.l <babushkin.a.l@yandex.ru>
2019-01-15 01:31:40 +0500

    Вернул как было

--- webapp/config.py
+++ webapp/config.py
@@ -6,5 +6,3 @@ SQLALCHEMY_DATABASE_URI="sqlite:///"+os.path.join(basedir,'..','webapp.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY="sdafsadfqw!@#!F,dfasasdfas25479"
-
-app.debug = True
