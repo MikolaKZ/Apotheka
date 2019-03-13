09133a7c3bd8e4f2ea20a2fe0199365e72f70b06
babushkin.a.l <babushkin.a.l@yandex.ru>
2019-03-13 23:39:22 +0500

    checked code

--- webapp/model.py
+++ webapp/model.py
@@ -1,19 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db=SQLAlchemy()
+db = SQLAlchemy()
+
 
 class User(db.Model, UserMixin):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(50),index=True,unique=True)
-    password=db.Column(db.String(128))
-    email=db.Column(db.String(128),unique=True)
-    dateRegistration=(db.String(10))
-    userTelegrammChat=db.Column(db.String(128),unique=True)
+    id = db.Column(db.Integer, primary_ke=True)
+    username = db.Column(db.String(50), index=True, unique=True)
+    password = db.Column(db.String(128))
+    email = db.Column(db.String(128), unique=True)
+    dateRegistration = (db.String(10))
+    userTelegrammChat = db.Column(db.String(128), unique=True)
 
     def set_password(self, password):
-        self.password=generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
@@ -21,13 +22,14 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
 class Profile(db.Model, UserMixin):
-     id=db.Column(db.Integer, primary_key=True)
-     age=db.Column(db.Integer)
-     name=db.Column(db.String(128), nullable=False)
-     sername=db.Column(db.String(128), nullable=False)
-     isWoman=db.Column(db.Boolean)
-     country=db.Column(db.String(128))
-     city=db.Column(db.String(128))
-     user_id=db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
-     Avatar=db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    age = db.Column(db.Integer)
+    name = db.Column(db.String(128), nullable=False)
+    sername = db.Column(db.String(128), nullable=False)
+    isWoman = db.Column(db.Boolean)
+    country = db.Column(db.String(128))
+    city = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    Avatar = db.Column(db.String)
