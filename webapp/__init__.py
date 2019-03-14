7ead88682083d523ac447eb2503f8213799f4c29
babushkin.a.l <babushkin.a.l@yandex.ru>
2019-03-14 23:59:13 +0500

    upgrade code

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,65 +1,61 @@
-# TODO поставить пробелы после запятых 
-# TODO строки слишком длинные, должны быть не более 79 символов
-
-from flask import Flask, render_template, flash, redirect, url_for,request, send_from_directory
-from flask_login import LoginManager, current_user, login_required, login_user, logout_user 
-from webapp.forms import LoginForm, RegistrationForm,PhotoForm,RegistrationFormWithoutPassword
-from webapp.model import db,User,Profile
-from webapp.otherFunc import str_to_bool 
+from flask import Flask, render_template, flash, redirect, url_for, request, send_from_directory
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from flask_migrate import Migrate
+from webapp.forms import LoginForm, RegistrationForm, PhotoForm, RegistrationFormWithoutPassword
+from webapp.model import db, User, Profile
+from webapp.otherFunc import str_to_bool
 from werkzeug.utils import secure_filename
 from werkzeug import SharedDataMiddleware
-import os, os.path
+import os
+import os.path
 from sqlalchemy.exc import IntegrityError
-from flask_migrate import Migrate
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
     db.init_app(app)
-    # TODO странный отступ, после и перед знаками равно должен быть пробел
-    login_manager =LoginManager()
+    login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view ="login"
+    login_manager.login_view = "login"
     migrate = Migrate(app, db)
-    
 
     @login_manager.user_loader
     def load_user(user_id):
-        # TODO странный отступ
-         return User.query.get(user_id)
+        return User.query.get(user_id)
 
     @app.route('/')
     def index():
         if current_user.is_authenticated:
-            return redirect(url_for('main')) 
+            return redirect(url_for('main'))
         return redirect(url_for('login'))
 
     @app.route('/content/main')
     def main():
-         title="Apotheka"
-         return render_template('/content/main.html',title=title) 
-   
+        title = "Apotheka"
+        return render_template('/content/main.html', title=title)
+
     @app.route('/user/login')
     def login():
         if current_user.is_authenticated:
-            return redirect(url_for('main')) 
-        title="Добро пожаловать в Apotheka"
-        login_form=LoginForm()
-        return render_template('/user/login.html',title=title,form=login_form)
+            return redirect(url_for('main'))
+        title = "Добро пожаловать в Apotheka"
+        login_form = LoginForm()
+        return render_template('/user/login.html', title=title, form=login_form)
 
-    @app.route('/user/regisration',methods = ['POST', 'GET'])
+    @app.route('/user/regisration', methods=['POST', 'GET'])
     def regisration():
-        title="Регистрация в Apotheka"
+        title = "Регистрация в Apotheka"
         login_form = RegistrationForm(request.form)
         if request.method == 'POST' and login_form.validate():
             # TODO очень нечитаемый список параметров
-            user = User(username=login_form.user_name.data,email=login_form.email.data,
-                       userTelegrammChat=request.form.get("telegram"))
-            try:    
+            user = User(username=login_form.user_name.data, email=login_form.email.data,
+                        userTelegrammChat=request.form.get("telegram"))
+            try:
                 user.set_password(login_form.password.data)
                 db.session.add(user)
                 db.session.commit()
-                profile=Profile(user_id=user.id, name=login_form.Name.data,sername=login_form.Sername.data,age=int(login_form.age.data),isWoman=str_to_bool(request.form.get("gender")))
+                profile = Profile(user_id=user.id, name=login_form.Name.data, sername=login_form.Sername.data, age=int(login_form.age.data), isWoman=str_to_bool(request.form.get("gender")))
                 db.session.add(profile)
                 db.session.commit()
                 flash('Спасибо за регистрацию')
@@ -67,47 +63,47 @@ def create_app():
             except IntegrityError:
                 flash('Пользователи с такими данными уже существуют')
                 return redirect(url_for('regisration'))
-        return render_template('/user/registration.html',title=title,form=login_form,User="",Profile="")
+        return render_template('/user/registration.html', title=title, form=login_form, User="", Profile="")
 
-    @app.route('/user/userProfile',methods = ['POST', 'GET'])
+    @app.route('/user/userProfile', methods=['POST', 'GET'])
     @login_required
     def userProfile():
-        title="Профайл пользователя"
+        title = "Профайл пользователя"
         login_form = RegistrationFormWithoutPassword(request.form)
-        photoForm=PhotoForm(request.form)
-        
+        photoForm = PhotoForm(request.form)
+
         if request.method == 'POST' and login_form.validate():
             profile = Profile.query.filter_by(user_id=current_user.get_id()).first()
             user = User.query.filter_by(id=current_user.get_id()).first()
-            user.username=login_form.user_name.data
-            user.email=login_form.email.data
-            user.userTelegrammChat=request.form.get("telegram")
+            user.username = login_form.user_name.data
+            user.email = login_form.email.data
+            user.userTelegrammChat = request.form.get("telegram")
             db.session.commit()
-            profile.name=login_form.Name.data
-            profile.country=request.form.get("country")
-            profile.city=request.form.get("city")
-            profile.sername=login_form.Sername.data
-            profile.age=int(login_form.age.data)
-            profile.isWoman=str_to_bool(request.form.get("gender"))
+            profile.name = login_form.Name.data
+            profile.country = request.form.get("country")
+            profile.city = request.form.get("city")
+            profile.sername = login_form.Sername.data
+            profile.age = int(login_form.age.data)
+            profile.isWoman = str_to_bool(request.form.get("gender"))
             db.session.commit()
             flash('Данные успешно перезаписаны')
             return redirect(url_for('login'))
-    
+
         profile = Profile.query.filter_by(user_id=current_user.get_id()).first()
-        user= User.query.filter_by(id=current_user.get_id()).first()
-        return render_template('/user/userProfile.html',title=title,form=login_form,photoForm=photoForm,User=user,Profile=profile)
-        
-    @app.route('/process_login',methods = ['POST'])
+        user = User.query.filter_by(id=current_user.get_id()).first()
+        return render_template('/user/userProfile.html', title=title, form=login_form, photoForm=photoForm, User=user, Profile=profile)
+
+    @app.route('/process_login', methods=['POST'])
     def process_login():
-        form=LoginForm()
+        form = LoginForm()
         if form.validate_on_submit():
             user = User.query.filter_by(username=form.username.data).first()
             if user and user.check_password(form.password.data):
                 login_user(user, remember=form.remember_me.data)
                 flash("Вы успешно зашли на сайт")
                 return redirect(url_for('userProfile'))
-            flash ("Неправильное имя и пароль") 
-            return redirect(url_for('login'))   
+            flash("Неправильное имя и пароль")
+            return redirect(url_for('login'))
 
     @app.route('/uploads', methods=['GET', 'POST'])
     def upload():
@@ -115,22 +111,21 @@ def create_app():
         if form.validate_on_submit():
             f = form.photo.data
             filename = secure_filename(f.filename)
-            file_obj=filename[-3:]
-            filename='avatar.'+file_obj
-            avatar_path=os.path.join(app.config["UPLOAD_FOLDER"],current_user.get_id(), filename)
+            file_obj = filename[-3:]
+            filename = 'avatar.'+file_obj
+            avatar_path = os.path.join(app.config["UPLOAD_FOLDER"], current_user.get_id(), filename)
             try:
                 f.save(avatar_path)
             except (FileNotFoundError):
-                os.makedirs(os.path.join(app.config["UPLOAD_FOLDER"],current_user.get_id()))
-                f.save(avatar_path)  
+                os.makedirs(os.path.join(app.config["UPLOAD_FOLDER"], current_user.get_id()))
+                f.save(avatar_path)
             profile = Profile.query.filter_by(user_id=current_user.get_id()).first()
-            profile.Avatar =avatar_path
+            profile.Avatar = avatar_path
             db.session.commit()
         return redirect(url_for('userProfile'))
 
     @app.route('/uploads/<filename>')
     def uploaded_image(filename):
-        return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'],current_user.get_id()),
-                                filename)
-         
+        return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], current_user.get_id()),
+                                   filename)
     return app
